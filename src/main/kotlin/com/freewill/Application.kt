f8a25953cd868d739187308731e13ff78aa1617a
Hyunseok Ko <56003992+lcomment@users.noreply.github.com>
2023-09-01 15:53:54 +0900

    Setting: #1 Setting Project
    * setting: create template
    
    * setting: create ci workflow yml
    
    * setting: set gitignore
    
    * Initialize Spring Boot Project 🎉
    
    * docs: upload google codestyle file
    
    * chore: add dependency and set checkstyle, jacoco
    
    * setting: set application yml file

---
+++ src/main/kotlin/com/freewill/Application.kt
@@ -0,0 +1,11 @@
+package com.freewill
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreeWillApplication
+
+fun main(args: Array<String>) {
+	runApplication<FreeWillApplication>(*args)
+}
